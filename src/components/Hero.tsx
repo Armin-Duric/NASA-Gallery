67d3ded99eecd26d354c6061efa42b9eb596921c
Armin <muttenroshi11@hotmail.com>
2024-04-15 00:55:11 +0200

    Upgrade Home page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,8 +3,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 // const API_KEY = "qfOWfkJtpXgrET2eHXbyagjS9trMUzL7ARYJvYfj";
-let searchWord = "milkey way";
-let page = 1;
+let searchWord = "nebula";
+let page = Math.floor(Math.random() * 4);
 const URL = `https://images-api.nasa.gov/search?q=${searchWord}&media_type=image&page=${page}`;
 
 const Hero: React.FC = () => {
@@ -27,16 +27,16 @@ const Hero: React.FC = () => {
     }
   }, []);
 
-  if (!images) {
+  if (!images.length) {
     return (
-      <h1 className="flex m-6 p-6 justify-center items-center text-center">
-        Loading...
+      <h1 className="lg:text-4xl md:text-2xl text-sm flex justify-center items-center m-4 p-4 md:m-10 md:p-20 indie-flower-regular">
+        Sorry, nothing to display yet.
       </h1>
     );
   }
 
-  if (images.length !== 0) {
-    return (
+  return (
+    <>
       <div className="grid w-fit md:grid-cols-3 2xl:grid-cols-4 grid-cols-2 grid-flow-row gap-6 m-4 overflow-hidden">
         {images.map((image: any, index) => (
           <div className="flex flex-col mb-6 overflow-hidden items-center w-full h-96 hover:scale-125 transition-transform duration-500">
@@ -52,14 +52,8 @@ const Hero: React.FC = () => {
           </div>
         ))}
       </div>
-    );
-  } else {
-    return (
-      <h1 className="lg:text-4xl md:text-2xl text-sm flex justify-center items-center m-4 p-4 md:m-10 md:p-20 indie-flower-regular">
-        Sorry, there are no result for this search.
-      </h1>
-    );
-  }
+    </>
+  );
 };
 
 export default Hero;
---
+++ src/components/NextPage.tsx
@@ -0,0 +1,16 @@
+
+
+const NextPage: React.FC = (page) => {
+
+  const handleClickNext: React.FC = () => {
+    if (page >= 1) {
+      page++;
+      console.log(page);
+    }
+  };
+
+  return (
+  )
+};
+
+export default NextPage;
