684acc5c5cea6988e76ba7664f5331fe0443e71e
MuttenRoshi11 <137054683+MuttenRoshi11@users.noreply.github.com>
2024-04-15 12:58:22 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
-import Pod from "./pages/PodPage";
+import PodPage from "./pages/PodPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
